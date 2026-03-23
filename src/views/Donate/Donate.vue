e6f6fa280c439c831b7b1f39fdb9b3cdac21d98e
Hgnim <hgn1m@outlook.com>
2026-03-23 19:30:39 +0800

    赞助页面优化

--- src/views/Donate/Donate.vue
+++ src/views/Donate/Donate.vue
@@ -2,8 +2,7 @@
 import {autoUseI18n} from "@/utils/i18nUtils.ts";
 import autoLoadLocale from "@/ts/global/vue/autoLoadLocale.ts";
 import {useTitle} from "@vueuse/core";
-import {type Ref, ref} from "vue";
-import staticDataAddress from "@/ts/env/staticDataAddress.ts";
+import patronsData_load from "@/views/Donate/ts/patronsData_load.ts";
 
 const {gt:t}=autoUseI18n();
 const lp:string="view_Donate";
@@ -11,81 +10,7 @@ autoLoadLocale(lp,()=>{
   useTitle(t(`${lp}.title`));
 });
 
-const patronsData:Ref<string[]> = ref([]);
-const patronsData_isLoaded:Ref<boolean> = ref(false);
-(async ()=>{
-  const res=await fetch(`${staticDataAddress}/donate/patron/data.json`);
-  if (res.ok){
-    const resJson = await res.json();
-
-
-    type SimplJson={
-      patrons:{
-        name:string,
-        allValue:number,
-      }[]
-    }
-    //简化的json
-    let simplJson:SimplJson={patrons:[]};
-    //排序的json
-    let sortJson:SimplJson={patrons:[]};
-
-    resJson.patron.forEach((item:any)=>{//为simplJson赋值
-      simplJson.patrons.push({
-        name: item.name,
-        allValue: (()=>{
-          let outputValue:number=0;
-          item.donateData.forEach((ite:any)=>{
-            outputValue+=Number(ite.value as string);
-          });
-          return outputValue;
-        })(),
-      })
-    });
-
-    {//为sortJson赋值
-      let pushedIndex:number[]=[];
-      while (simplJson.patrons.length != pushedIndex.length) {
-        let maxValIndex: number = -1;
-        {
-          let lastMax = 0;
-          for (let i = 0; i < simplJson.patrons.length; i++) {
-            {
-              let isCon=false;
-              for (let j=0;j<pushedIndex.length; j++){
-                if (pushedIndex[j]==i) {
-                  isCon = true;
-                  break;
-                }
-              }
-              if (isCon)
-                continue;
-            }
-            if (simplJson.patrons[i]!.allValue > lastMax) {
-              lastMax = simplJson.patrons[i]!.allValue;
-              maxValIndex = i;
-            }
-          }
-        }
-        sortJson.patrons.push(simplJson.patrons[maxValIndex]!);
-        pushedIndex.push(maxValIndex);
-      }
-    }
-
-    {//将最终值赋值至patronsData
-      let output:string[]=[];
-      sortJson.patrons.forEach((item) => {
-        output.push(item.name);
-      });
-      patronsData.value=output;
-    }
-
-    patronsData_isLoaded.value=true;
-  }else{
-    patronsData_isLoaded.value=true;
-    patronsData.value=[t(`${lp}.error`)];
-  }
-})();
+const {patronsData,patronsData_isLoaded,patronsData_isFailed} = patronsData_load();
 </script>
 
 <template>
@@ -94,12 +19,10 @@ const patronsData_isLoaded:Ref<boolean> = ref(false);
       <div class="row">
         <div class="col-12 d-flex justify-content-center align-items-center">
           <div id="main-title" class="text-center">
-            <h1>
-              <svg id="title-svg-0" class="title-svg"><use xlink:href="#svg-logo-mjyy-logo"></use></svg>
-              {{t(`${lp}.mainTitle.h1`)}}
-              <svg id="title-svg-1" class="title-svg"><use xlink:href="#svg-logo-mjyy-logo"></use></svg>
-            </h1>
-            <h3>{{t(`${lp}.mainTitle.h3`)}}</h3>
+            <svg id="title-svg-0" class="title-svg"><use xlink:href="#svg-logo-mjyy-logo"></use></svg>
+            <h1>{{t(`${lp}.mainTitle.h1`)}}</h1>
+            <h4>{{t(`${lp}.mainTitle.h4`)}}</h4>
+            <svg id="title-svg-1" class="title-svg"><use xlink:href="#svg-logo-mjyy-logo"></use></svg>
           </div>
         </div>
       </div>
@@ -123,6 +46,11 @@ const patronsData_isLoaded:Ref<boolean> = ref(false);
               <span class="visually-hidden"></span>
             </div>
           </div>
+        </div>
+        <div class="col-12 text-center"
+             :style="(patronsData_isFailed==true)?{}:{display:'none'}"
+        >
+          <strong>{{t(`${lp}.failed`)}}</strong>
         </div>
 
         <div class="col-6 col-sm-4 col-md-3 col-xl-2 mx-auto text-center"
@@ -153,6 +81,7 @@ const patronsData_isLoaded:Ref<boolean> = ref(false);
       position: absolute;
       z-index: -1;
       fill: var(--main-logo-color);
+      top: 0;
     }
 
     #title-svg-0 {
